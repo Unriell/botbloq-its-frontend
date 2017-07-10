d72407a77046426e92ab73886c26893293d3a845
Eimardsobrino <eimardsobrinoz@gmail.com>
2017-07-10 20:46:04 +0200

    load pic - courses

--- app/scripts/controllers/courses.js
+++ app/scripts/controllers/courses.js
@@ -8,7 +8,7 @@
  * Controller of the botbloqItsFrontendApp
  */
 
-   botBloqApp.controller('coursesCtrl',function($log,$q,$scope,$http,$location,coursesApi,usersApi,lomsApi,common) {
+   botBloqApp.controller('coursesCtrl',function($log,$q,$scope,$http,$location,$timeout,coursesApi,usersApi,lomsApi,common) {
         $log.log('courses ctrl start');
         
         $scope.activeUser=common.activeUSer;
@@ -749,6 +749,27 @@
 
          // ----------------    FIN STUDENT METHODS ---------------
 
-    
+        // -------------- LOAD IMAGES METHODS ---------------------
+        $scope.thumbnail = {
+            dataUrl: common.bitbloqBackendUrladsfas
+        };
+        $scope.fileReaderSupported = window.FileReader != null;
+        $scope.photoChanged = function(files){
+            if (files != null) {
+                var file = files[0];
+                if ($scope.fileReaderSupported && file.type.indexOf('image') > -1) {
+                    $timeout(function() {
+                        var fileReader = new FileReader();
+                        fileReader.readAsDataURL(file);
+                        fileReader.onload = function(e) {
+                            $timeout(function(){
+                                $scope.thumbnail.dataUrl = e.target.result;
+                            });
+                        }
+                    });
+                }
+            }
+        };
+        // 
 
     });
